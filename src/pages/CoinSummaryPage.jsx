0c5f6c17576de745dd7315ad2783ec9969c6a6c4
MaySalmon <58439353+MaySalmon@users.noreply.github.com>
2021-05-11 18:41:26 +0300

    check

--- src/pages/CoinSummaryPage.jsx
+++ src/pages/CoinSummaryPage.jsx
@@ -4,6 +4,7 @@ import CoinList from "../components/CoinList";
 const CoinSummaryPage = () => {
   return (
     <div className="coinsummary shadow border p-2 rounded mt-2 bg-light">
+     <h1>Hello world</h1>
       <CoinList />
     </div>
   );
